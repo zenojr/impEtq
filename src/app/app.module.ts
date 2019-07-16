8db63ca9d24d395c331b142e57c2abf01c0425fa
zenojr <zfrancajunior@gmail.com>
2019-07-16 13:54:53 -0300

    added impressao component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImpressaoComponent } from './impressao/impressao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImpressaoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/impressao/impressao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-impressao',
+  templateUrl: './impressao.component.html',
+  styleUrls: ['./impressao.component.css']
+})
+export class ImpressaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
