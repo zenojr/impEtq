263f0fd58744759caaf64c9df355396675ae8f72
zenojr <zfrancajunior@gmail.com>
2019-07-25 14:20:53 -0300

    added pipes, chage layot flex

---
+++ src/app/impressao/produto.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'produto'
+})
+export class ProdutoPipe implements PipeTransform {
+
+  transform(value: any, ...args: any[]): any {
+    let dataOut: string;
+    if ( value === 'prodpadr' ) {
+      dataOut =  'Produto padrão';
+    } else if ( value === 'cordpar' ) {
+      dataOut = 'Cordão Paralelo Torcido';
+    } else {
+      dataOut = 'Cobre Nú';
+    }
+    return dataOut;
+  }
+
+}
+
