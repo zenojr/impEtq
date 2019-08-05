fec5950d1312be98b1abf6d6ab4ec5dd7e75f4f3
zenojr <zfrancajunior@gmail.com>
2019-08-05 15:47:18 -0300

    screeen works

--- src/app/impressoras/impressoras.component.ts
+++ src/app/impressoras/impressoras.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-impressoras',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImpressorasComponent implements OnInit {
 
-  constructor() { }
+  selectedProd = null;
+  selectedTipoProd = null;
+  dados: any;
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  reinspecao = false;
+  checked = false;
+
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit() {
+
+    this.firstFormGroup = this.formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this.formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+
   }
 
 }
